ba4b8dfee9b9727d8aaa0520b128c98a91049671
wliuzhu <893438903@qq.com>
2016-02-29 21:53:19 +0800

    第一次

--- app/src/main/java/com/view/mytest/MainActivity.java
+++ app/src/main/java/com/view/mytest/MainActivity.java
@@ -1,13 +1,26 @@
 package com.view.mytest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView)findViewById(R.id.text_main);
+        text.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(this, "ddddd", Toast.LENGTH_SHORT).show();
     }
 }
